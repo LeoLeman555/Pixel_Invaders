40e2a51327ff9e5dc2e1a37991372b1b4ba3b46f
Léo Leman <leo.leman555@gmail.com>
2025-01-25 10:24:15 +0100

    feat(main): add the game window and a main ship that can move

--- main.py
+++ main.py
@@ -1,3 +1,50 @@
-import pyxel
+import pyxel as px
 
-print("Hello world !")
+SCREEN_SIZE = 128
+SHIP_SIZE = (11, 16)
+
+class Main:
+    def __init__(self):
+        """Initialize the game and load resources."""
+        px.init(SCREEN_SIZE, SCREEN_SIZE, "Pixel Invaders")
+
+        # Load the ship image
+        px.image(0).load(0, 0, "assets/images/player_ship.png")
+
+        self.x = SCREEN_SIZE // 2
+        self.y = SCREEN_SIZE // 2
+        self.run()
+
+    def update(self):
+        """Update game logic."""
+        self.move()
+
+    def move(self):
+        """Handle player movement."""
+        if px.btn(px.KEY_W) or px.btn(px.KEY_UP):
+            if self.y > 0:
+                self.y -= 1
+        if px.btn(px.KEY_A) or px.btn(px.KEY_LEFT):
+            if self.x > 0:
+                self.x -= 1
+        if px.btn(px.KEY_S) or px.btn(px.KEY_DOWN):
+            if self.y < SCREEN_SIZE - SHIP_SIZE[1]:
+                self.y += 1
+        if px.btn(px.KEY_D) or px.btn(px.KEY_RIGHT):
+            if self.x < SCREEN_SIZE - SHIP_SIZE[0]:
+                self.x += 1
+
+    def draw(self):
+        """Render game elements on the screen."""
+        px.cls(0)
+        # Draw the ship
+        px.blt(self.x, self.y, 0, 0, 0, SHIP_SIZE[0], SHIP_SIZE[1] + 2)
+        # Draw the x and the y for debugging
+        px.text(0, 0, f"{self.x, self.y}", 7)
+
+    def run(self):
+        """Start the game loop."""
+        px.run(self.update, self.draw)
+
+if __name__ == "__main__":
+    Main()
