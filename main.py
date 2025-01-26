8bdf920f612d8aa87eb05e26c601ceb1e556f4c4
Léo Leman <leo.leman555@gmail.com>
2025-01-26 21:39:30 +0100

    feat(laser): add laser that shoots from the ship

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyxel as px
+from source.laser import *
 
 SCREEN_SIZE = 128
 SHIP_SIZE = (11, 16)
@@ -10,13 +11,19 @@ class Main:
 
         # Load the ship image
         px.image(0).load(0, 0, "assets/images/player_ship.png")
-
         self.x = SCREEN_SIZE // 2
         self.y = SCREEN_SIZE // 2
+
+        self.laser_manager = LaserManager()
+
         self.run()
 
     def update(self):
         """Update game logic."""
+        if px.btnp(px.KEY_SPACE):
+            self.laser_manager.create_laser(1, 3, 10, self.x, self.y, SHIP_SIZE[0])
+        self.laser_manager.move_lasers()
+
         self.move()
 
     def move(self):
@@ -37,10 +44,9 @@ class Main:
     def draw(self):
         """Render game elements on the screen."""
         px.cls(0)
-        # Draw the ship
+        self.laser_manager.draw_lasers()
         px.blt(self.x, self.y, 0, 0, 0, SHIP_SIZE[0], SHIP_SIZE[1] + 2)
-        # Draw the x and the y for debugging
-        px.text(0, 0, f"{self.x, self.y}", 7)
+        px.text(0, 0, f"{self.x, self.y}", 7) # Draw the x and the y for debugging
 
     def run(self):
         """Start the game loop."""
