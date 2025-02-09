b5857345a9c84837b3a41efb882caf053c6000fd
Léo Leman <leo.leman555@gmail.com>
2025-02-09 21:13:48 +0100

    [Game] Enhance mechanics: boosters, waves, enemies, refactoring (#4)
    * feat(player): improve visual display of lives and optimize ship movement and position
    
    * feat(boosts): add 5 different game boosters
    
    Added five distinct boosters: speed, rapid fire, extra life, big shoot, and enemies slow. Each booster has unique effects and timers to improve gameplay variety.
    
    * feat(waves): add wave mechanics to increase difficulty
    
    * feat(game): refactor entity management, optimize sprites, and add enemy attacks
    
    Refactored entity management by replacing dictionaries with object-oriented structures:
    - Introduced EnemyManager and Enemy classes for better encapsulation.
    - Applied the same approach to boosts (BoostManager / Boost) and projectiles (Player, Laser, Missile).
    
    Optimized sprite handling by grouping assets into three spritesheets (Player, Boost, Enemies).
    
    Enhanced gameplay with:
    - A second enemy type.
    - Enemy laser attacks, adding more challenge and dynamism.

--- main.py
+++ main.py
@@ -1,18 +1,58 @@
 import pyxel as px
-from source.enemies import *
-from source.shooting import *
+from source.boosts_manager import *
+from source.enemies_manager import *
+from source.player import *
+from source.shooting_manager import *
 
 
 class Main:
-    SCREEN_SIZE = 128
-    SHIP_SIZE = (11, 16)
-
     def __init__(self):
-        """Initialize the game and load resources."""
-        px.init(self.SCREEN_SIZE, self.SCREEN_SIZE, "Pixel Invaders")
+        """Initialize and start the game: Pixel Invaders."""
+        px.init(128, 128, "Pixel Invaders")
 
-        # Load the ship image
-        px.image(0).load(0, 0, "assets/images/player_ship.png")
+        self.waves_data = [
+            [1, 1, 0],
+            [2, 1, 1],
+            [3, 1, 0],
+            [4, 1, 0],
+            [5, 1, 1],
+            [6, 1, 0],
+            [3, 2, 1],
+            [7, 1, 0],
+            [8, 1, 1],
+            [4, 2, 0],
+            [9, 1, 1],
+            [5, 2, 0],
+            [10, 1, 0],
+            [6, 2, 1],
+            [3, 3, 0],
+            [7, 2, 0],
+            [4, 3, 1],
+            [8, 2, 0],
+            [5, 3, 1],
+            [9, 2, 0],
+            [6, 3, 0],
+            [10, 2, 1],
+            [7, 3, 0],
+            [8, 3, 1],
+            [6, 4, 0],
+            [9, 3, 0],
+            [6, 4, 1],
+            [10, 3, 0],
+            [7, 4, 0],
+            [8, 4, 0],
+            [9, 4, 1],
+            [10, 4, 0],
+            [7, 5, 0],
+            [8, 5, 1],
+            [9, 5, 0],
+            [10, 5, 1],
+        ]
+
+        # Load the all the images
+        px.images[0].load(0, 0, "assets/images/player_spritesheet.png")
+        px.images[1].load(0, 0, "assets/images/enemies_spritesheet.png")
+        px.images[2].load(0, 0, "assets/images/boosts_spritesheet.png")
 
         self.reset_game()
 
@@ -20,19 +60,18 @@ class Main:
 
     def reset_game(self):
         """Reset game variables for a new game."""
-        self.x = self.SCREEN_SIZE // 2
-        self.y = self.SCREEN_SIZE // 2
-        self.lives = 3
         self.score = 0
+        self.wave = 0
+        self.max_wave = len(self.waves_data)
         self.game_state = "playing"  # Can be 'playing' or 'game_over'
 
-        self.shooting_count = 0
-        self.heat = 0  # Heat gauge
-        self.max_heat = 10  # Overheating threshold
-        self.cooldown = 0  # Waiting time in the event of overheating
+        self.enemies_speed = 1
+        self.extra_life_given = False
 
+        self.player = Player(self, px.width // 2, px.height)
         self.shooting_manager = ShootingManager(self)
         self.enemies_manager = EnemiesManager(self)
+        self.boosts_manager = BoostsManager(self)
 
     def update(self):
         """Update game logic."""
@@ -43,30 +82,10 @@ class Main:
 
     def update_playing(self):
         """Update logic while the game is running."""
-        if not self.enemies_manager.enemies:
-            self.enemies_manager.create_wave(0, 5, 2)
-
-        if px.btnp(px.KEY_SPACE) and self.heat < self.max_heat and self.cooldown == 0:
-            if self.shooting_count % 20 == 10:
-                self.shooting_manager.create_missile(
-                    self.x, self.y + self.SHIP_SIZE[0] // 2
-                )
-                self.shooting_manager.create_missile(
-                    self.x + self.SHIP_SIZE[0], self.y + self.SHIP_SIZE[0] // 2
-                )
-            else:
-                self.shooting_manager.create_laser(
-                    1, 3, 10, self.x, self.y, self.SHIP_SIZE[0]
-                )
-            self.shooting_count += 1
-            self.heat += 2  # Each shot increases the heat
-
-        self.shooting_manager.overheating()
-        self.shooting_manager.move_lasers()
-        self.shooting_manager.move_missiles()
-        self.enemies_manager.move_enemies()
-        self.enemies_manager.delete_enemy()
-        self.move()
+        self.player.update()
+        self.boosts_manager.update()
+        self.enemies_manager.update()
+        self.shooting_manager.update()
 
     def update_game_over(self):
         """Handle input during the game over screen."""
@@ -74,27 +93,6 @@ class Main:
             self.reset_game()
         if px.btnp(px.KEY_Q):
             px.quit()
-
-    def move(self):
-        """Handle player movement."""
-        if px.btn(px.KEY_W) or px.btn(px.KEY_UP):
-            if self.y > 0:
-                self.y -= 1
-        if px.btn(px.KEY_A) or px.btn(px.KEY_LEFT):
-            if self.x > 0:
-                self.x -= 1
-        if px.btn(px.KEY_S) or px.btn(px.KEY_DOWN):
-            if self.y < self.SCREEN_SIZE - self.SHIP_SIZE[1]:
-                self.y += 1
-        if px.btn(px.KEY_D) or px.btn(px.KEY_RIGHT):
-            if self.x < self.SCREEN_SIZE - self.SHIP_SIZE[0]:
-                self.x += 1
-
-    def lose_life(self):
-        """Reduce player lives and check for game over."""
-        self.lives -= 1
-        if self.lives <= 0:
-            self.game_state = "game_over"
 
     def draw(self):
         """Render game elements on the screen."""
@@ -106,31 +104,30 @@ class Main:
 
     def draw_playing(self):
         """Draw the game screen."""
-        px.blt(self.x, self.y, 0, 0, 0, self.SHIP_SIZE[0], self.SHIP_SIZE[1] + 2)
-        self.shooting_manager.draw_overheating()
-        self.shooting_manager.draw_lasers()
-        self.shooting_manager.draw_missiles()
-        self.enemies_manager.draw_enemies()
+        self.shooting_manager.draw()
+        self.enemies_manager.draw()
+        self.boosts_manager.draw()
+        self.player.draw()
 
-        # Management of flashing if lives are at 1
-        if self.lives == 1:
-            # Flashes red every 10 frames (alternating between red and invisible)
-            if (px.frame_count // 10) % 2 == 0:
-                px.text(2, 2, f"LIVES : {self.lives}", px.COLOR_RED)
-        else:
-            # Displays lives normally
-            px.text(2, 2, f"LIVES : {self.lives}", 7)
+        # Displays lives
+        if self.player.lives > 1 or (
+            self.player.lives == 1 and (px.frame_count // 10) % 2 == 0
+        ):
+            for i in range(self.player.lives):
+                px.blt(1 + i * 8, 1, 2, 80, 0, 8, 8, 0)
 
         # Displays the score
         px.text(75, 2, f"SCORE : {self.score}", 7)
+        px.text(75, 10, f"WAVE : {self.wave}", 7)
 
     def draw_game_over(self):
         """Draw the game over screen."""
         if (px.frame_count // 10) % 2 == 0:
-            px.text(30, 20, "--- GAME OVER ---", 7)
-            px.text(30, 40, f"SCORE: {self.score}", 7)
             px.text(2, 110, "PRESS 'R' TO RESTART", 7)
             px.text(2, 120, "PRESS 'Q' TO QUIT", 7)
+        px.text(30, 20, "--- GAME OVER ---", 7)
+        px.text(30, 40, f"SCORE: {self.score}", 7)
+        px.text(30, 50, f"WAVE : {self.wave}", 7)
 
     def run(self):
         """Start the game loop."""
